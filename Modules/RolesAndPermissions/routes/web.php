fb2f4e5f03da048c549ab12d75278dd6455c1542
Lana Septiana <lana.septiana2@gmail.com>
2025-01-25 23:05:36 +0700

    fix(roles and permissions): fix the route permissions for the roles and permissions

--- Modules/RolesAndPermissions/routes/web.php
+++ Modules/RolesAndPermissions/routes/web.php
@@ -16,17 +16,30 @@ use Modules\RolesAndPermissions\Http\Controllers\RolesAndPermissionsController;
 
 Route::prefix('admin')->middleware(['redirect.if.not.authenticated'])->group(function () {
     Route::prefix('roles-and-permissions')->group(function () {
-        Route::get('/', [RolesAndPermissionsController::class, 'index'])->name('roles.and.permissions.index');
-        Route::get('create', [RolesAndPermissionsController::class, 'create'])->name('roles.and.permissions.create');
-        Route::post('/', [RolesAndPermissionsController::class, 'store'])->name('roles.and.permissions.store');
-        Route::get('{role_and_permission}', [RolesAndPermissionsController::class, 'show'])->name('roles.and.permissions.show');
-        Route::get('{role_and_permission}/edit', [RolesAndPermissionsController::class, 'edit'])->name('roles.and.permissions.edit');
-        Route::put('{role_and_permission}', [RolesAndPermissionsController::class, 'update'])->name('roles.and.permissions.update');
-        Route::delete('{role_and_permission}', [RolesAndPermissionsController::class, 'destroy'])->name('roles.and.permissions.destroy');
+        Route::get('/', [RolesAndPermissionsController::class, 'index'])
+            ->middleware('can:view_roles_and_permissions')
+            ->name('roles.and.permissions.index');
 
-        // Save table settings route within roles-and-permissions prefix
+        Route::get('create', [RolesAndPermissionsController::class, 'create'])
+            ->middleware('can:create_roles_and_permissions')
+            ->name('roles.and.permissions.create');
+        Route::post('/', [RolesAndPermissionsController::class, 'store'])
+            ->middleware('can:create_roles_and_permissions')
+            ->name('roles.and.permissions.store');
+
+        Route::get('{role_and_permission}/edit', [RolesAndPermissionsController::class, 'edit'])
+            ->middleware('can:edit_roles_and_permissions')
+            ->name('roles.and.permissions.edit');
+        Route::put('{role_and_permission}', [RolesAndPermissionsController::class, 'update'])
+            ->middleware('can:edit_roles_and_permissions')
+            ->name('roles.and.permissions.update');
+
+        Route::delete('{role_and_permission}', [RolesAndPermissionsController::class, 'destroy'])
+            ->middleware('can:delete_roles_and_permissions')
+            ->name('roles.and.permissions.destroy');
+
         Route::post('save-table-settings', [RolesAndPermissionsController::class, 'saveTableSettings'])
-            ->middleware('can:can_update_roles_and_permissions_table_settings')
+            ->middleware('can:update_roles_and_permissions_table_settings')
             ->name('roles.and.permissions.save.table.settings');
     });
 });
